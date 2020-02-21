0d709ce4bb61e041cbb521abe9ea64b6545b6e81
Yevhenii Shevchenko <yevhenii.shevchenko@altexsoft.com>
2020-02-21 15:11:21 +0200

    fix incorrect name css classes and minuses margin

--- src/components/layouts/TheFooter/TheFooter.vue
+++ src/components/layouts/TheFooter/TheFooter.vue
@@ -1,38 +1,38 @@
 <template>
   <footer class="footer">
     <div class="footer-wrapper">
-      <div class="footer-item">
-        <p class="item-website-title">This website is for demo purposes only</p>
-        <p class="item-website-description">
-          It demonstrates the functionality of AltexSoft Booking Engine – integration with GDS, flight search, booking
-          and ticketing process. You cannot book real flight ticket here.
+      <div>
+        <p class="footer-title">This website is for demo purposes only</p>
+        <p class="footer-description">
+          It demonstrates the functionality of AltexSoft Booking Engine – integration with
+          GDS, flight search, booking and ticketing process. You cannot book real flight
+          ticket here.
         </p>
       </div>
-      <div class="footer-item">
-        <a class="item-ref" href="/">
-          <img class="ref-logo" src="../../../assets/images/logo.svg" alt="Logo" />
-          <img class="ref-logo-desktop" src="../../../assets/images/desktop-logo.svg" alt="Logo" />
+      <div>
+        <a href="/">
+          <img class="ref-logo-mobile" src="../../../assets/images/logo.svg" alt="Logo" />
+          <img
+            class="ref-logo-desktop"
+            src="../../../assets/images/desktop-logo.svg"
+            alt="Logo-desktop"
+          />
         </a>
-        <div class="item-description-company">
+        <div class="description-company">
           <p>Travel Technology Consulting company</p>
-          <a class="description-company-ref" href="#">altexsoft.com</a>
+          <a class="company-ref" href="#">altexsoft.com</a>
         </div>
       </div>
       <div>
-        <a class="contact-phone-ref" href="tel:+1 (877) 777-9097">+1 (877) 777-9097</a>
-        <a class="contact-email-ref" href="mailto:traveltech@altexsoft.com">traveltech@altexsoft.com</a>
+        <a class="phone-ref" href="tel:+1 (877) 777-9097">+1 (877) 777-9097</a>
+        <a class="email-ref" href="mailto:traveltech@altexsoft.com">traveltech@altexsoft.com</a>
       </div>
-      <div class="footer-item">
+      <div>
         <p>© Copyright 2019</p>
       </div>
-      <div class="footer-item">
-        <div class="item-contact">
-          <a class="contact-phone-ref" href="tel:+1 (877) 777-9097">+1 (877) 777-9097</a>
-          <a
-            class="contact-email-ref"
-            href="mailto:traveltech@altexsoft.com"
-          >traveltech@altexsoft.com</a>
-        </div>
+      <div>
+        <a class="phone-ref" href="tel:+1 (877) 777-9097">+1 (877) 777-9097</a>
+        <a class="email-ref" href="mailto:traveltech@altexsoft.com">traveltech@altexsoft.com</a>
       </div>
     </div>
   </footer>
