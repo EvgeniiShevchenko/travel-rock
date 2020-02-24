1033122eee5d5011b33c1efaa4c226c5debca894
Yevhenii Shevchenko <yevhenii.shevchenko@altexsoft.com>
2020-02-24 19:44:44 +0200

    footer fix errors

--- src/components/layouts/Footer/TheFooter.vue
+++ src/components/layouts/Footer/TheFooter.vue
@@ -1,39 +1,36 @@
 <template>
-  <footer class="footer">
-    <div class="footer-wrapper container">
-      <div class="about-us">
-        <p class="about-us-title">This website is for demo purposes only</p>
-        <p class="about-us-description">
-          It demonstrates the functionality of AltexSoft Booking Engine – integration with GDS, flight search, booking
-          and ticketing process. You cannot book real flight ticket here.
-        </p>
-      </div>
-      <div class="general">
-        <a href="/">
-          <img class="ref-logo-mobile" src="../../../assets/images/logo.svg" alt="Logo" />
-          <img
-            class="ref-logo-desktop"
-            src="../../../assets/images/desktop-logo.svg"
-            alt="Logo-desktop"
-          />
-        </a>
-        <div class="description-company">
-          <p>Travel Technology Consulting company</p>
-          <a class="company-ref" href="#">altexsoft.com</a>
-        </div>
-      </div>
-      <div class="contacts-mobile">
-        <a class="phone-ref" href="tel:+1 (877) 777-9097">+1 (877) 777-9097</a>
-        <a class="email-ref" href="mailto:traveltech@altexsoft.com">traveltech@altexsoft.com</a>
-      </div>
-      <div class="copyrights">
-        <p>© Copyright 2019</p>
-      </div>
-      <div class="contacts-desktop col-xs-1">
-        <a class="phone-ref" href="tel:+1 (877) 777-9097">+1 (877) 777-9097</a>
-        <a class="email-ref" href="mailto:traveltech@altexsoft.com">traveltech@altexsoft.com</a>
+  <footer class="footer container">
+    <!-- <div class="footer-wrapper container"> -->
+    <div class="about-us">
+      <p class="title">This website is for demo purposes only</p>
+      <p class="description">
+        It demonstrates the functionality of AltexSoft Booking Engine –
+        integration with GDS, flight search, booking and ticketing process.
+        You cannot book real flight ticket here.
+      </p>
+    </div>
+    <div class="general">
+      <a href="/">
+        <img class="logo-mobile" src="../../../assets/images/logo.svg" alt="Logo-mobile" />
+        <img class="logo-desktop" src="../../../assets/images/desktop-logo.svg" alt="Logo-desktop" />
+      </a>
+      <div class="description-company">
+        <p>Travel Technology Consulting company</p>
+        <a class="company" href="#">altexsoft.com</a>
       </div>
     </div>
+    <div class="contacts-mobile">
+      <a class="phone" href="tel:+1 (877) 777-9097">+1 (877) 777-9097</a>
+      <a class="email" href="mailto:traveltech@altexsoft.com">traveltech@altexsoft.com</a>
+    </div>
+    <div class="copyrights">
+      <p>© Copyright 2019</p>
+    </div>
+    <div class="contacts-desktop">
+      <a class="phone" href="tel:+1 (877) 777-9097">+1 (877) 777-9097</a>
+      <a class="email" href="mailto:traveltech@altexsoft.com">traveltech@altexsoft.com</a>
+    </div>
+    <!-- </div> -->
   </footer>
 </template>
 
