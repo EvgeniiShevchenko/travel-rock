1ae2bcb369dee629e424fba441a9f4d91c3f179b
Svitlana Sakharylenko <svitlana.sakharylenko@altexsoft.com>
2020-03-20 01:10:16 +0200

    fix select

--- src/components/modules/Select/Select.js
+++ src/components/modules/Select/Select.js
@@ -3,22 +3,16 @@ import Multiselect from 'vue-multiselect';
 export default {
   name: 'Select',
   components: { Multiselect },
-  data() {
-    return {
-      isActive: false,
-      value: 'Economy',
-    }
-  },
   props: {
     options: {
-      type: [],
-      default: ['Economy', 'Premium Economy', 'Business', 'First'],
+      type: Array,
+      default: () => []
+    },
+    value: {
+      type: String
     }
   },
   methods: {
-    arrowToggle: function () {
-      this.isActive = !this.isActive;
-    },
     changeValue() {
       this.$emit('changeValue', this.value);
     }
--- src/components/modules/Select/Select.vue
+++ src/components/modules/Select/Select.vue
@@ -1,26 +1,14 @@
 <template>
-  <div :class="{ 'is-focused': isActive }"
-       @click="arrowToggle"
+  <Multiselect v-model="value"
+               :options="options"
+               :searchable="false"
+               :show-labels="false"
+               @input="changeValue"
   >
-    <Multiselect v-model="value"
-                 :options="options"
-                 :searchable="false"
-                :show-labels="false"
-                 @input="changeValue"
-    >
-      <span class="select-arrow"
-            slot="caret"
-            slot-scope="{ toggle }"
-            @mousedown.prevent.stop="toggle"
-      >
-      </span>
-    </Multiselect>
-  </div>
+  </Multiselect>
 </template>
 
 <script src='./Select.js'></script>
-
-<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
 
 <style lang="scss">
   @import "./Select.scss";
